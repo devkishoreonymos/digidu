2449762561f0d66d8d8cea9ef151d33d0377f4f1
rohit chaudhary <rrohitcchaudhary@gmail.com>
2018-10-11 18:19:55 +0530

    added email functionality

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require("express");
+var bodyParser = require('body-parser');
 var todoController = require("./controllers/todoController");
+const digiRoute = require('./routes/route');
 //var ejs     = require("ejs");
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.set('view engine','ejs');
 app.use(express.static('./public'));
+app.use('/', digiRoute);
 
-
-todoController(app);
+//todoController(app);
 
 
 app.listen(3000,()=>{
